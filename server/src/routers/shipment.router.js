04d04a48eeb482fc82ed6dd4fa6967af65a234bc
Anton Öhman <anton.ohman@hotmail.com>
2020-05-28 13:00:42 +0200

    shipments and payments

--- server/src/routers/shipment.router.js
+++ server/src/routers/shipment.router.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
-// const { Shipment } = require("../models/shipment.model");
-
 /* MIDDLEWARES */
 const isAuthenticated = require("../middlewares/isAuthenticated");
 
