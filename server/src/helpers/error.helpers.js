33522d02db6b2ac43f9376d2febd7651d800d4ef
ChristianAgren <55452324+ChristianAgren@users.noreply.github.com>
2020-06-03 11:23:09 +0200

    Created errorhandler

---
+++ server/src/helpers/error.helpers.js
@@ -0,0 +1,24 @@
+class ErrorHandler extends Error {
+    constructor(statusCode, message) {
+        super()
+        this.statusCode = statusCode
+        this.message = message
+    }
+
+}
+const handleError = (err, res) => {    
+    let { statusCode, message } = err
+    if (!statusCode) statusCode = 500
+    if (!message) message = "Unhandled Server Error"
+    res.status(statusCode).json({
+        status: "error",
+        statusCode,
+        message
+    })
+    console.error(err)
+}
+
+module.exports = {
+    ErrorHandler,
+    handleError
+}
