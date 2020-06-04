2a24d9b1753738434690e7bc38e5275684f0822a
Anne-Lie Bäck <56996804+Anne-Lie-Back@users.noreply.github.com>
2020-06-04 14:41:21 +0200

    modal is showing at the right place

--- client/src/components/cart.tsx
+++ client/src/components/cart.tsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, {useContext, useState} from "react";
 
 import { Link } from "react-router-dom";
+
+import AuthenticationContext from "../contexts/authentication-context/context";
 
 import { Box, Button, Heading } from "grommet";
 import { Close } from "grommet-icons";
 
 import CartItems from "./cart-items";
+import SignInSignUpModal from "./signInSignUpModal"
 
 interface Iprops {
   closeCart: () => void;
 }
 
 const MyCart = (props: Iprops) => {
+  
+  const [showModal, setShowModal] = useState(false)
+  const { isAuthenticated } = useContext(AuthenticationContext);
+
+  const checkSession = () => {
+    if(!isAuthenticated){
+      setShowModal(true)
+    }else{
+      setShowModal(false)
+      props.closeCart()
+    }
+  }
+
+
   return (
+    <>
+    {showModal? <SignInSignUpModal/>:null }
+    {/* {isAuthenticated?null:<SignInSignUpModal/>} */}
     <Box>
       <Box align="center" height="100vh" overflow="auto">
         <Button alignSelf="end" icon={<Close />} onClick={props.closeCart} />
@@ -28,10 +48,11 @@ const MyCart = (props: Iprops) => {
           margin="medium"
           primary
           label="Proceed to checkout"
-          onClick={props.closeCart}
+          onClick={checkSession}
         />
       </Link>
     </Box>
+    </>
   );
 };
 
